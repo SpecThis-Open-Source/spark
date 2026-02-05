790277832418c9ae26e735c806991815c9846e2d
Adam Holsinger <adamdude828@gmail.com>
2026-02-05 13:35:08 -0700

    initial commit

---
+++ src/main/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow(): void {
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 700,
+    minWidth: 480,
+    minHeight: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(app.getAppPath(), 'src', 'renderer', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
